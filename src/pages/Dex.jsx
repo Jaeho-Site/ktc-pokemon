d7b27de259fe12e51bc425dc3c9e9ed324fddbce
Jaeho <sjaeho348@gmail.com>
2025-05-24 15:58:25 +0900

    feat : 필수구현3 alert띄우기

--- src/pages/Dex.jsx
+++ src/pages/Dex.jsx
@@ -7,17 +7,26 @@ const Dex = () => {
     const [selected,setSelected] = useState([]);
 
     const addPokemon = (pokemon) => {
+        if (selected.find(p => p.id === pokemon.id)) {
+            alert("이미 선택된 포켓몬입니다.");
+            return;
+        }     
+        if (selected.length >= 6) {
+            alert("더 이상 선택할 수 없습니다.");
+            return;
+        }
         setSelected([...selected, pokemon]);
-      };
+    };
+    
     const removePokemon = (pokemon) => {
         setSelected(selected.filter((p) => p !== pokemon));
     };
+    
     return (
         <>
             <Dashboard selected={selected} removePokemon={removePokemon} />
             <PokemonList pokemons={MOCK_DATA} addPokemon={addPokemon} />
         </>
-
     )
 };
 
